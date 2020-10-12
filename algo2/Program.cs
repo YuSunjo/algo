54c4fe6f48dbda4dcf2b2f3dcfac78f17a7ef7a4
yusunjo <tnswh2023@gmail.com>
2020-10-12 19:24:49 +0900

    algo2일차

---
+++ algo2/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+		{
+			//홀짝 판별
+			//string input;
+			//input = Console.ReadLine();
+
+			//int num = int.Parse(input);
+
+			//if (num % 2 == 0)
+			//{
+			//	Console.WriteLine("even");
+			//}
+			//else if(num%2 ==1)
+			//{
+			//	Console.WriteLine("odd");
+			//         }
+			//         else
+			//         {
+			//	Console.WriteLine("잘못입력했습니다.");
+			//         }
+
+			//
+			//string input1, input2, input3;
+			//input1 = Console.ReadLine();
+			//input2 = Console.ReadLine();
+			//input3 = Console.ReadLine();
+
+			//float korean = float.Parse(input1);
+			//float math = float.Parse(input2);
+			//float english = float.Parse(input3);
+
+			//float avg = (korean + math + english) / 3;
+			//string grade;
+
+			//if(avg >= 90)
+			//         {
+			//	grade = "A";
+			//         }
+			//else if(avg>=80)
+			//         {
+			//	grade = "B";
+			//         }
+			//else if (avg >= 70)
+			//{
+			//	grade = "C";
+			//}
+			//else if (avg >= 60)
+			//{
+			//	grade = "D";
+			//}
+			//else
+			//         {
+			//	grade = "F";
+			//         }
+
+			//Console.WriteLine($"{avg:F2},{grade}");
+
+			string[] token;
+			string str;
+			string grade;
+			double[] tab = new double[10];
+
+			double tot = 0, avg;
+
+			str = null;
+			str = Console.ReadLine();
+
+			token = str.Split(' ');
+			for(int j=0; j<token.Length; j++)
+            {
+				token[j].Trim();
+            }
+			for(int j=0; j<token.Length; j++)
+            {
+				tab[j] = Convert.ToDouble(token[j]);
+            }
+			
+			for(int j=0; j<token.Length; j++)
+            {
+				tot += tab[j];
+            }
+			avg = tot / token.Length;
+
+            if (avg >= 90)
+            {
+                grade = "A";
+            }
+            else if (avg >= 80)
+            {
+                grade = "B";
+            }
+            else if (avg >= 70)
+            {
+                grade = "C";
+            }
+            else if (avg >= 60)
+            {
+                grade = "D";
+            }
+            else
+            {
+                grade = "F";
+            }
+
+			Console.WriteLine($"{avg:F2} {grade}");
+
+
+
+
+
+        }
+	}
+}
---
+++ algor/소스.cpp
@@ -0,0 +1,43 @@
+//약수 구하기
+//#include <stdio.h>
+//int main() {
+//
+//	int num;
+//	scanf("%d", &num);
+//
+//	for (int i = 1; i <= num; i++)
+//	{
+//		if (num % i == 0) {
+//			printf("%d ", i);
+//		}
+//	}
+//
+//	return 0;
+//}
+
+//3의 배수와 5의 배수들의 합 
+//#include <stdio.h>
+//int main() {
+//
+//	int num;
+//	int sum3 = 0;
+//	int sum5 = 0;
+//	int sum15=0;
+//
+//	scanf_s("%d", &num);
+//
+//	for (int i = 0; i <= num / 3; i++) {
+//		sum3 += 3 * i;
+//	}
+//	for (int i = 0; i <= num / 5; i++) {
+//		sum5 += 5 * i;
+//	}
+//	for (int i = 0; i <= num / 15; i++) {
+//		sum15 += 15 * i;
+//	}
+//
+//	printf("%d", sum3 + sum5 - sum15);
+//
+//	return 0;
+//}
+
