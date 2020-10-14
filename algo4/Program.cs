0ce425d083f7677cdfb95d0369c770b7db3488e2
yusunjo <tnswh2023@gmail.com>
2020-10-14 14:48:53 +0900

    algo 4일차 -고장난 컴퓨터, 태민이의 취미

---
+++ algo4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace algo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int main()
+            //{
+            //    long number;
+            //    cin >> number;
+
+            //    long sum = number * (number + 1) / 2 % 1000000007;
+            //    long answer = sum * sum % 1000000007;
+
+            //    cout << answer;
+            //    return 0;
+            //}
+
+            //1부터 n까지 부피의 합 -- for문 쓰면 값이 커져서 시간 초과 
+            //string str;
+            //str = Console.ReadLine();
+            //long number = long.Parse(str);
+            //long sum = number * (number + 1) /2 % 1000000007;
+            //long answer = sum * sum % 1000000007;
+
+            //Console.WriteLine(answer);
+
+            //고장난 컴퓨터 
+            //int count, time;
+            //int num = 1;
+            //cin >> count >> time;
+
+            //int arr[100000] = { 0 };
+            //for (int i = 0; i < count; i++)
+            //{
+            //    cin >> arr[i];
+            //}
+
+            //for (int i = 0; i < count - 1; i++)
+            //{
+
+
+            //}
+
+            //cout << num;
+
+
+        }
+    }
+}
