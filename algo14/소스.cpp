d2ee913b077fdbeafbc3f7b67218f8457f8cc106
yusunjo <tnswh2023@gmail.com>
2021-01-13 23:39:26 +0900

    백준 그리디 알고리즘 -동전0, 거스름돈, 로프

--- algo14/소스.cpp
+++ algo14/소스.cpp
@@ -35,53 +35,85 @@
 //}
 
 //백준 그리디 알고리즘 - 거스름돈 
+//
+//#include<iostream>
+//#include<vector>
+//
+//using namespace std;
+//int main() {
+//	
+//	int num;
+//	cin >> num;
+//	
+//	int count = 0;
+//	int left;
+//	left = 1000 - num;
+//
+//	while (left > 0) {
+//		if (left % 500 == 0) {
+//			left -= 500;
+//			count++;
+//		}
+//		else if (left % 100 == 0)
+//		{
+//			left -= 100;
+//			count++;
+//		}
+//		else if (left % 50 == 0) 
+//		{
+//			left -= 50;
+//			count++;
+//		}
+//		else if (left % 10 == 0)
+//		{
+//			left -= 10;
+//			count++;
+//		}
+//		else if (left % 5 == 0)
+//		{
+//			left -= 5;
+//			count++;
+//		}
+//		else if (left % 1 == 0)
+//		{
+//			left -= 1;
+//			count++;
+//		}
+//
+//	}
+//
+//	cout << count;
+//}
+
+//백준 그리디 알고리즘 - 로프
 
 #include<iostream>
 #include<vector>
+#include <algorithm>
 
 using namespace std;
+
 int main() {
-	
 	int num;
+
 	cin >> num;
-	
-	int count = 0;
-	int left;
-	left = 1000 - num;
-
-	while (left > 0) {
-		if (left % 500 == 0) {
-			left -= 500;
-			count++;
+	vector<int> v;
+	int rof=0;
+	int max = 0;
+	for (int i = 0; i < num; i++)
+	{
+		cin >> rof;
+		v.push_back(rof);
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < num; i++)
+	{
+		int var = v[num - 1 - i] * (i + 1);
+		if (var > max) {
+			max = var;
 		}
-		else if (left % 100 == 0)
-		{
-			left -= 100;
-			count++;
-		}
-		else if (left % 50 == 0) 
-		{
-			left -= 50;
-			count++;
-		}
-		else if (left % 10 == 0)
-		{
-			left -= 10;
-			count++;
-		}
-		else if (left % 5 == 0)
-		{
-			left -= 5;
-			count++;
-		}
-		else if (left % 1 == 0)
-		{
-			left -= 1;
-			count++;
-		}
-
 	}
 
-	cout << count;
+	cout << max;
 
 }
